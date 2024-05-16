3f66641a7e002bbbed3e05a1e93c178e648719b8
bintang4 <43073401+bintang4@users.noreply.github.com>
2024-05-16 11:01:23 +0700

    Add files via upload

---
+++ un.py
@@ -0,0 +1,37 @@
+import os,json
+from re import findall as reg
+
+def sct(so):
+  acc_sid = so.replace("/.env","").replace("www.","").replace("mail.","").replace("webmail.","").replace("ns.","").replace("http://", "").replace("https://", "")
+  files = open('coi.txt', 'a')
+  files.write(str(acc_sid)+'\n')
+  files.close()
+def scg(so):
+  keyid = so[1]
+  secret = so[2]
+  buid = str(keyid)+'|'+str(secret)
+  remoer = str(buid).replace('\n', '')
+  files = open('ws.txt', 'a')
+  files.write(remoer+'\n')
+  files.close()
+  
+def scgt(so):
+  keyid = so[1]
+  secret = so[2]
+  keyid2 = so[3]
+  secret2 = so[4]
+  buid = str(keyid2)+'|'+str(secret2)
+  remoer = str(buid).replace('\n', '')
+  files = open('ws.txt', 'a')
+  files.write(remoer+'\n')
+  files.close()
+
+nam = raw_input('List Ips: ')
+with open(nam) as f:
+    for site in f:
+     #try:
+         so = site.split("|")
+         scgt(so)
+         #sct(site)
+     #except:
+            #pass
